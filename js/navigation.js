ef3bf7a893b96512f4ee80fae81926f175b3498a
LeeYongSeok13 <minamiharuka333@gmail.com>
2026-09-11 23:35:14 +0900

    코드 정리 v.1

--- js/navigation.js
+++
@@ -1,6 +0,0 @@
-const intro = document.querySelector("#intro");
-const enterBtn = document.querySelector(".enter_btn");
-
-enterBtn.addEventListener("click", () => {
-  intro.classList.add("hide");
-});
