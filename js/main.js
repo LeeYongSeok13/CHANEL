282514cffaa4c76a02b76abd586c0d043eaf7ed6
LeeYongSeok13 <minamiharuka333@gmail.com>
2026-09-12 21:01:48 +0900

    세션 스토리지 인트로 최적화

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 const intro = document.querySelector("#intro");
 const enterBtn = document.querySelector(".enter_btn");
 
+const introShown = sessionStorage.getItem("introShown");
+
+// 인트로 화면에서 "Enter" 버튼 클릭 시 인트로 숨기기
 enterBtn.addEventListener("click", () => {
   intro.classList.add("hide");
 });
+
+if (introShown === "true") {
+  // 이미 봤다면 인트로 숨기기
+  intro.style.display = "none";
+} else {
+  // 처음이라면 방문 기록 저장하기
+  sessionStorage.setItem("introShown", "true");
+}
+
