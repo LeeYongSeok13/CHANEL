34e0f84f373cce7af409673f932a52e74eb4c9de
LeeYongSeok13 <minamiharuka333@gmail.com>
2026-09-26 22:19:09 +0900

    메뉴 구성 다시하기

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const introShown = sessionStorage.getItem("introShown");
 
 // 인트로 화면에서 "Enter" 버튼 클릭 시 인트로 숨기기
 enterBtn.addEventListener("click", () => {
+  // 처음이라면 방문 기록 저장하기
+  sessionStorage.setItem("introShown", "true");
   intro.classList.add("hide");
   // 인트로가 완전히 사라지면 슬라이드 시작
   intro.addEventListener("transitionend", function onIntroHidden(event) {
@@ -18,7 +20,4 @@ if (introShown === "true") {
   // 이미 봤다면 인트로 숨기기
   intro.style.display = "none";
   startSlider();
-} else {
-  // 처음이라면 방문 기록 저장하기
-  sessionStorage.setItem("introShown", "true");
 }
